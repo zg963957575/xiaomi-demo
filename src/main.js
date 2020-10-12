bf0cbc55298a6965c719b0bb79fa3adefd63312e
zg <2016958467@qq.com>
2020-10-12 09:49:04 +0800

    创建项目结构

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import VueRouter from "vue-router";
 import Vant from "vant";
 import LyTab from "ly-tab";
 import VueLazyload from "vue-lazyload";
+import axios from "axios";
 
 import "vant/lib/index.css";
 import "./assets/css/reset.css";
@@ -17,6 +18,7 @@ VueRouter.prototype.replace = function replace(location) {
   return originalReplace.call(this, location).catch(err => err);
 };
 
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 Vue.use(Vant);
 Vue.use(LyTab);
